bd26275c961e1076c7c03955de22c9b36edb354e
caizhengyi <910660298@qq.com>
2021-04-12 19:46:45 +0800

    修改部分代码——用类封装、换变量名

--- succeed.py
+++ succeed.py
@@ -4,198 +4,189 @@
 # Author: Rex
 # Time: 2020/4/26-11:44
 # Author: Rex
+
+"""
+df1是11点之前
+df2是11点之后
+df3是要查询的对应文件
+df4是将df1和df2拼起来的文件
+处理时直接处理df4
+df5是输出保存的excel
+"""
 from datetime import datetime
 
 import pandas as pd
 
-file1 = r'C:\Users\Administrator\Desktop\yllCsv\df1.csv'
-file2 = r'C:\Users\Administrator\Desktop\yllCsv\df2.csv'
-file3 = r'C:\Users\Administrator\Desktop\yllCsv\df3.csv'
-file4 = r'C:\Users\Administrator\Desktop\yllCsv\df4.csv'
-result = r'C:\Users\Administrator\Desktop\yllCsv\result.csv'
-result2 = r'C:\Users\Administrator\Desktop\yllCsv\result2.csv'
-
-# df1 = pd.read_csv(file1, encoding='gbk')
-# df2 = pd.read_csv(file2, encoding='gbk')
-# df3 = pd.read_csv(file3, encoding='gbk')
-# df4 = pd.read_csv(file4)
-df5 = pd.read_csv(result)
-
 first = True
 
 
-def list_to_dict(lst):
-    '''
-    :param list: 重复元素的列表。ls = ['a', 'b', 'c', 'a']
-    :return: {'a': [0, 3], 'b': [1], 'c': [2]}
+class Deal():
+    def __init__(self):
+        file1 = 'static/csvFiles/bus1.csv'
+        file2 = 'static/csvFiles/bus2.csv'
+        file3 = 'static/csvFiles/subway.csv'
+        file4 = 'static/csvFiles/bus_all.csv'
+        result = 'static/csvFiles/result.csv'
 
-    '''
-    dic = {}
-    for value in lst:
-        dic[value] = []
-    for index, value in enumerate(lst):
-        dic[value].append(index)
+        self.bus1 = pd.read_csv(file1, encoding='gbk')
+        self.bus2 = pd.read_csv(file2, encoding='gbk')
+        self.subway = pd.read_csv(file3, encoding='gbk')
+        self.bus_all = pd.read_csv(file4)
+        self.result_csv = pd.read_csv(result)
 
-    return dic
+        self.dic_in_df2 = self._list_to_dict(self.bus_all.iloc[:, 0])
+        self.dic_in_df5 = self._list_to_dict(self.result_csv.iloc[:, 0])
 
+    def _list_to_dict(self, lst):
+        """
+        返回：{元素:[元素对应的位置]}
+        :param list: 重复元素的列表。ls = ['a', 'b', 'c', 'a']
+        :return: {'a': [0, 3], 'b': [1], 'c': [2]}
 
-# dic_in_df2 = list_to_dict(df4.iloc[:, 0])
-dic_in_df5 = list_to_dict(df5.iloc[:, 0])
+        """
+        dic = {}
+        for value in lst:
+            dic[value] = []
+        for index, value in enumerate(lst):
+            dic[value].append(index)
 
+        return dic
 
-def find_indexes(card_id):
-    # 从df4中找idCard，返回index列表，没找到返回空列表
-    # 先建立哈希表
+    def _find_indexes(self, card_id):
+        # 从df4中找idCard，返回index列表，没找到返回空列表
+        # 先建立哈希表
 
-    if card_id in dic_in_df2.keys():
-        return dic_in_df2[card_id]
-    else:
-        return []
-
-
-def save(file, row3, row4):
-    '''
-    :param file: 保存到的文件地址
-    :param row3: 地铁下车的行信息
-    :param row4: 公交上车的行信息
-    :return:
-    '''
-    global first
-    dic = {
-        '卡号': [row3[0]],
-        '地铁卡种': [row3[1]],
-        '地铁交易日期': [row3[2]],
-        '地铁交易时间': [row3[3]],
-        '地铁商户名称': [row3[4]],
-        '地铁进出站': [row3[5]],
-        '地铁机具编号': [row3[6]],
-        '地铁线路编号': [row3[7]],
-        '地铁车辆编号': [row3[8]],
-        '地铁站点编号': [row3[9]],
-        '地铁站名称': [row3[10]],
-
-        '公交卡种': [row4[1]],
-        '公交交易日期': [row4[2]],
-        '公交交易时间': [row4[3]],
-        '公交商户名称': [row4[4]],
-        '公交进出站': [row4[5]],
-        '公交机具编号': [row4[6]],
-        '公交线路编号': [row4[7]],
-        '公交车辆编号': [row4[8]],
-        '公交线路': [row4[9]]
-    }
-    df = pd.DataFrame(dic)
-
-    if first == True:
-        df.to_csv(file, mode='w', header=True, index=False)
-        first = False
-    else:
-        df.to_csv(file, mode='a', header=False, index=False)
-
-
-def find_close_time_in_df(df, indexes_in_df4, t1):
-    flag = False  # 是否找到过一个
-
-    now_index = None
-    for index in indexes_in_df4:
-        row = df.iloc[index, :]
-        # 第一次找到之前比较只需看是否在地铁时间之后
-        if flag == False:
-            if t1 < datetime.strptime(row[3], "%H:%M:%S"):
-                now_close_time = datetime.strptime(row[3], "%H:%M:%S")
-                now_index = index
-                flag = True
-        # 第二次之后的比较，需要与
+        if card_id in self.dic_in_df2.keys():
+            return self.dic_in_df2[card_id]
         else:
-            if t1 < datetime.strptime(row[3], "%H:%M:%S") < now_close_time:
-                now_close_time = datetime.strptime(row[3], "%H:%M:%S")
-                now_index = index
+            return []
 
-    return now_index
+    def _save(self, file, row_subway, row_bus):
+        """
+        :param file: 保存到的文件地址
+        :param row_subway: 地铁下车的行信息
+        :param row_bus: 公交上车的行信息
+        :return:
+        """
+        global first
+        dic = {
+            '卡号': [row_subway[0]],
+            '地铁卡种': [row_subway[1]],
+            '地铁交易日期': [row_subway[2]],
+            '地铁交易时间': [row_subway[3]],
+            '地铁商户名称': [row_subway[4]],
+            '地铁进出站': [row_subway[5]],
+            '地铁机具编号': [row_subway[6]],
+            '地铁线路编号': [row_subway[7]],
+            '地铁车辆编号': [row_subway[8]],
+            '地铁站点编号': [row_subway[9]],
+            '地铁站名称': [row_subway[10]],
 
+            '公交卡种': [row_bus[1]],
+            '公交交易日期': [row_bus[2]],
+            '公交交易时间': [row_bus[3]],
+            '公交商户名称': [row_bus[4]],
+            '公交进出站': [row_bus[5]],
+            '公交机具编号': [row_bus[6]],
+            '公交线路编号': [row_bus[7]],
+            '公交车辆编号': [row_bus[8]],
+            '公交线路': [row_bus[9]]
+        }
+        df = pd.DataFrame(dic)
 
-def main(fromIndex):
-    global first
-    if fromIndex == 0:
-        first = True
-    else:
-        first = False
-    for index, row in df3.iterrows():
-        if index >= fromIndex:
+        if first == True:
+            df.to_csv(file, mode='w', header=True, index=False)
+            first = False
+        else:
+            df.to_csv(file, mode='a', header=False, index=False)
+
+    def _find_closest_time(self, df, indexes_in_df4, time):
+        # 在df中找到与time最接近的时间，并返回这一行的index
+        flag = False  # 是否找到过一个
+
+        now_index = None
+        for index in indexes_in_df4:
+            row = df.iloc[index, :]
+            # 第一次找到之前比较只需看是否在地铁时间之后
+            if flag == False:
+                if time < datetime.strptime(row[3], "%H:%M:%S"):
+                    now_close_time = datetime.strptime(row[3], "%H:%M:%S")
+                    now_index = index
+                    flag = True
+            # 第二次之后的比较，需要与
+            else:
+                if time < datetime.strptime(row[3], "%H:%M:%S") < now_close_time:
+                    now_close_time = datetime.strptime(row[3], "%H:%M:%S")
+                    now_index = index
+
+        return now_index
+
+    def run(self):
+        for index, row in self.subway.iterrows():
             card_id = row[0]
             # 从df4中找idCard，返回index列表，没找到返回空列表
-            indexes_in_df4 = find_indexes(card_id)
+            indexes_df4 = self._find_indexes(card_id)
             # 在df4中找到了
-            if len(indexes_in_df4) != 0:
-                t1 = datetime.strptime(row[3], "%H:%M:%S")
+            if len(indexes_df4) != 0:
+
+                subway_time = datetime.strptime(row[3], "%H:%M:%S")  # 地铁出站时间
                 # 找出在df4中位置为indexes_in_df4的时间与t1最近的那行的index,没找到返回None
-                close_index = find_close_time_in_df(df4, indexes_in_df4, t1)
+                close_index = self._find_closest_time(self.bus_all, indexes_df4, subway_time)
                 if close_index != None:
-                    save(result, row, df4.iloc[close_index, :])
+                    self._save(self.result, row, self.bus_all.iloc[close_index, :])
                     print('第', index, '行写入成功！')
                 else:
                     print('卡号：', row[0], '公交上车时间都在地铁时间之前')
-                # t2 = datetime.strptime(t2_str, "%H:%M:%S")
-                # for index2 in indexes_in_df4:
-                #     # 地铁下车时间
-                #     t1 = datetime.strptime(row[3], "%H:%M:%S")
-                #     # 公交上车时间
-                #
-                #     t2 = datetime.strptime(df4.iloc[index2, 3], "%H:%M:%S")
-                #     # 公交时间在地铁时间之后
-                #     if t2 > t1:
-                #         save(result, row, df4.iloc[index2, :])
-                #         print('第', index, '行写入成功！')
-                #         break
-                # else:
-                #     print('卡号：', row[0], '公交上车时间都在地铁时间之前')
             else:
                 print('没找到第', index, '行的id', row[0])
 
 
-def get_max_index(indexes):
-    max_index = indexes[0]
-    max_time = datetime.strptime(df5.iloc[indexes[0], 3], "%H:%M:%S")
-    for index in indexes:
-        time2 = datetime.strptime(df5.iloc[index, 3], "%H:%M:%S")
-        if time2 > max_time:
-            max_time = time2
-            max_index = index
-    return max_index
+
+class OtherCode():
+
+    def get_max_index(indexes):
+        max_index = indexes[0]
+        max_time = datetime.strptime(df5.iloc[indexes[0], 3], "%H:%M:%S")
+        for index in indexes:
+            time2 = datetime.strptime(df5.iloc[index, 3], "%H:%M:%S")
+            if time2 > max_time:
+                max_time = time2
+                max_index = index
+        return max_index
 
 
-def drop_not_max_index(indexes):
-    # global df5
-    delete_index = []
-    max_index = get_max_index(indexes)
-    for index in indexes:
-        if index != max_index:
-            delete_index.append(index)
+    def drop_not_max_index(indexes):
+        # global result_csv
+        delete_index = []
+        max_index = get_max_index(indexes)
+        for index in indexes:
+            if index != max_index:
+                delete_index.append(index)
 
-    return delete_index
+        return delete_index
 
 
-def main2():
-    i = 0
-    delete_index = []
-    for card_id, indexes in dic_in_df5.items():
+    def main2():
+        i = 0
+        delete_index = []
+        for card_id, indexes in dic_in_df5.items():
 
-        if len(indexes) > 1:
-            item_delete_index = drop_not_max_index(indexes)
+            if len(indexes) > 1:
+                item_delete_index = drop_not_max_index(indexes)
 
-            delete_index.extend(item_delete_index)
-        print('第', i, '行处理完毕')
-        i += 1
+                delete_index.extend(item_delete_index)
+            print('第', i, '行处理完毕')
+            i += 1
 
-    print(delete_index)
-    df = df5.drop(index=delete_index)
-    print('开始写入')
-    df.to_csv(result2, mode='w', header=True, index=False)
+        print(delete_index)
+        df = df5.drop(index=delete_index)
+        print('开始写入')
+        df.to_csv(result2, mode='w', header=True, index=False)
 
 
 if __name__ == '__main__':
     # main(0)
-    print(dic_in_df5)
-    print(len(df5))
-    print(df5.iloc[27578, :])
+    # print(dic_in_df5)
+    # print(len(result_csv))
+    # print(result_csv.iloc[27578, :])
     main2()
